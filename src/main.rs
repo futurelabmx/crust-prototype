bab0193956bf687fe8eabcd2788f8e2c5f601115
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-05-26 15:48:55 -0500

    Finished main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+extern crate libc;
+
+use std::env;
+use std::ffi::CString;
+use libc::{c_int, c_char};
+
+extern "C" {
+    fn main_c(argc: c_int, argv: *const *const c_char) -> c_int;
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Get the current arguments  and map them into a vector
+    let args: Vec<Cstring> = env::args().filter_map(|arg| {
+        CString::new(arg).ok
+    }).collect();
+
+    // Convertir las CStrings a apuntadores
+    let c_args: Vec<*const c_char> = args.iter().map(|arg| {
+        arg.as_ptr()
+    }).collect();
+
+    // Call the main function within the created c library
+    unsafe {
+        main_c(c_args.len() as c_int, c_args.as_ptr());
+    };
 }
